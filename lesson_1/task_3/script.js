6779f4b533564b18a0160a8cb8e0c62ba88d75e3
Vladimir Novik <v.novik45@gmail.com>
2021-09-21 13:18:02 +0300

    Complete lesson_1

---
+++ lesson_1/task_3/script.js
@@ -0,0 +1,14 @@
+// 3. Объявить две переменные: admin и name. Записать в name строку "Василий"; 
+// Скопировать значение из name в admin. 
+// Вывести admin (должно вывести «Василий»).
+
+
+let admin, 
+    uname;
+
+
+uname = 'Василий';
+admin = uname;
+
+
+document.write('admin= ', admin);
